5284b196ea872f03da44a7f71ce9e989aed954b9
IbrahimKhanGH <iboo11khan@gmail.com>
2024-10-26 17:58:47 -0500

    dashboard init:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,28 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/layout/Navbar'; // Import Navbar
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import Retirement from './pages/Retirement';
-import IncomeExpenses from './pages/ExpsenseTracker';
-import EducationResources from './pages/EducationResources';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Retirement from "./pages/Retirement";
+import IncomeExpenses from "./pages/ExpsenseTracker";
+import EducationResources from "./pages/EducationResources";
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar /> {/* Navbar is now imported here */}
-        {/* Main Content */}
-        <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <div className="flex flex-col min-h-screen w-full bg-gray-50">
+        <Navbar />
+        <main className="flex-grow w-full">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/retirement" element={<Retirement />} />
             <Route path="/income-expenses" element={<IncomeExpenses />} />
-            <Route path="/EducationResources" element={<EducationResources/>} />
+            <Route
+              path="/educationresources"
+              element={<EducationResources />}
+            />
           </Routes>
         </main>
       </div>
